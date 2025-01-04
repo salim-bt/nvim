ce7a85c46ffa2a7845ef15d525b2ab4e98479660
SALIM <salimpradhan.tech@gmail.com>
2025-01-04 14:01:52 +0600

    Add new plugins for enhanced editing experience and improved terminal management

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -1,4 +1,110 @@
 return {
+  -- Enhanced f/t motions
+  {
+    "ggandor/flit.nvim",
+    keys = function()
+      local ret = {}
+      for _, key in ipairs({ "f", "F", "t", "T" }) do
+        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = "nx" },
+  },
+  -- Enhanced leap motions
+  {
+    "ggandor/leap.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  -- Better increase/decrease
+  {
+    "monaqa/dial.nvim",
+    keys = {
+      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+    },
+    config = function()
+      local augend = require("dial.augend")
+      require("dial.config").augends:register_group({
+        default = {
+          augend.integer.alias.decimal,
+          augend.integer.alias.hex,
+          augend.date.alias["%Y/%m/%d"],
+          augend.constant.alias.bool,
+          augend.semver.alias.semver,
+        },
+      })
+    end,
+  },
+  -- Better text-objects
+  {
+    "chrisgrieser/nvim-various-textobjs",
+    lazy = false,
+    opts = { useDefaultKeymaps = true },
+  },
+  -- Auto-save files
+  {
+    "pocco81/auto-save.nvim",
+    event = { "InsertLeave", "TextChanged" },
+    opts = {
+      execution_message = {
+        enabled = false,
+      },
+      trigger_events = { -- See :h events
+        immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+        defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+        cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+      },
+    },
+  },
+  -- Better diagnostics list
+  {
+    "folke/trouble.nvim",
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { 
+      use_diagnostic_signs = true,
+      action_keys = {
+        close = "q",
+        cancel = "<esc>",
+        refresh = "r",
+        jump = "<cr>",
+        toggle_mode = "m",
+        toggle_preview = "P",
+        hover = "K",
+        preview = "p",
+        close_folds = {"zM", "zm"},
+        open_folds = {"zR", "zr"},
+        toggle_fold = {"zA", "za"},
+        previous = "k",
+        next = "j"
+      },
+    },
+    keys = {
+      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics" },
+      { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
+      { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List" },
+      { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List" },
+    },
+  },
+  -- Better marks
+  {
+    "chentoast/marks.nvim",
+    event = "BufReadPost",
+    config = true,
+  },
+  -- Better quickfix
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf",
+    opts = {},
+  },
   -- File explorer
   {
     "nvim-neo-tree/neo-tree.nvim",
@@ -36,36 +142,6 @@ return {
       { "<leader>sr", function() require("spectre").open() end, desc = "Replace in files (Spectre)" },
     },
   },
-  -- Better quickfix
-  {
-    "kevinhwang91/nvim-bqf",
-    ft = "qf",
-    opts = {},
-  },
-  -- Better marks
-  {
-    "chentoast/marks.nvim",
-    event = "BufReadPost",
-    config = true,
-  },
-  -- Better word motions
-  {
-    "chrisgrieser/nvim-various-textobjs",
-    lazy = false,
-    opts = { useDefaultKeymaps = true },
-  },
-  -- Better diagnostics list and others
-  {
-    "folke/trouble.nvim",
-    cmd = { "TroubleToggle", "Trouble" },
-    opts = { use_diagnostic_signs = true },
-    keys = {
-      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
-    },
-  },
   -- Todo comments
   {
     "folke/todo-comments.nvim",
@@ -79,6 +155,135 @@ return {
       { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
       { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
       { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme" },
+    },
+  },
+  -- Better comments
+  {
+    "numToStr/Comment.nvim",
+    opts = {
+      padding = true,
+      sticky = true,
+      toggler = {
+        line = 'gcc',
+        block = 'gbc',
+      },
+      opleader = {
+        line = 'gc',
+        block = 'gb',
+      },
+    },
+    lazy = false,
+  },
+  -- Better splits and windows
+  {
+    "mrjones2014/smart-splits.nvim",
+    opts = {
+      ignored_filetypes = {
+        'nofile',
+        'quickfix',
+        'prompt',
+      },
+      ignored_buftypes = { 'NvimTree' },
+    },
+    keys = {
+      { "<A-h>", function() require("smart-splits").resize_left() end, desc = "Resize left" },
+      { "<A-j>", function() require("smart-splits").resize_down() end, desc = "Resize down" },
+      { "<A-k>", function() require("smart-splits").resize_up() end, desc = "Resize up" },
+      { "<A-l>", function() require("smart-splits").resize_right() end, desc = "Resize right" },
+    },
+  },
+  -- Better terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+      size = 20,
+      open_mapping = [[<c-\>]],
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      persist_size = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+          border = "Normal",
+          background = "Normal",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+      
+      -- Custom terminal functions
+      local Terminal = require("toggleterm.terminal").Terminal
+      
+      -- Lazygit terminal
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "curved",
+        },
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+        end,
+      })
+
+      -- Node terminal
+      local node = Terminal:new({
+        cmd = "node",
+        direction = "float",
+        float_opts = {
+          border = "curved",
+        },
+      })
+
+      -- Python terminal
+      local python = Terminal:new({
+        cmd = "python3",
+        direction = "float",
+        float_opts = {
+          border = "curved",
+        },
+      })
+
+      -- Key mappings for custom terminals
+      vim.keymap.set("n", "<leader>tg", function() lazygit:toggle() end, {desc = "Lazygit"})
+      vim.keymap.set("n", "<leader>tn", function() node:toggle() end, {desc = "Node"})
+      vim.keymap.set("n", "<leader>tp", function() python:toggle() end, {desc = "Python"})
+      
+      -- Terminal navigation
+      function _G.set_terminal_keymaps()
+        local opts = {buffer = 0}
+        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+      end
+
+      -- Auto-apply terminal keymaps
+      vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+    end,
+  },
+  -- Session management
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } },
+    keys = {
+      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
   },
 } 
